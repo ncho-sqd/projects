6f1aef76e9c387a9d7fc470754c7fb0b1c69cd19
Cho, Neil <ncho@cornerstone.com>
2017-01-12 18:23:23 -0800

    Added functionality to collect OpCl return for all select ticker.

--- finance/data_download/download_skeleton.R
+++ finance/data_download/download_skeleton.R
@@ -1,13 +1,21 @@
+# Clear console and remove every object in the environment
+cat("\014")
+rm(list = ls())
+
 #Install and load packages
-package_list=c("quantmod")
+package_list=c("quantmod", "ggplot2")
 sapply(package_list,require, character.only=T)
 
 #Download market data
 market_tickers=c("^GSPC","^DJI","^KS11")
 markets<-getSymbols(market_tickers)
-ret<-head(merge(OpCl(GSPC),OpCl(DJI),all=F))
+
+market_returns <- do.call(cbind,lapply(markets,function(x) OpCl(get(x))))
+names(market_returns)<-paste(markets,"_RET",sep="")
+
 
 #Store market data
 str(ret)
 save(ret,file="D:/datasets/markets.xts")
-  
+
+
