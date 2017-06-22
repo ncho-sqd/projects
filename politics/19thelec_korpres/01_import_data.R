1ebba708f82bc88fa1909efe89c8837111493605
Neil <cjw.neil@gmail.com>
2017-06-21 21:52:13 -0700

    Replaced to readxl for xlsx import.

--- politics/19thelec_korpres/01_import_data.R
+++ politics/19thelec_korpres/01_import_data.R
@@ -1,2 +1,9 @@
-library(xlsx)
-read.xlsx("D:/Users/NCho/data.xlsx", sheetIndex = 1, colNames = TRUE)
+cat("\014")
+rm(list=ls())
+source("D:/git/header.R")
+
+library(readxl)
+filename <- "_19.xlsx"
+temp<-read_xlsx(paste0(importdir,"/projects/politics/19thelec_korpres/",filename), 
+          sheet = 1, 
+          col_names = TRUE)
