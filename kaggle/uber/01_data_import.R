7adc342cfa6500591206b94fd8fe2fafdb17e027
Cho, Neil <ncho@cornerstone.com>
2017-08-10 16:26:40 -0700

    Updated uber import code.

--- kaggle/uber/01_data_import.R
+++ kaggle/uber/01_data_import.R
@@ -3,6 +3,14 @@ cat('\014')
 rm(list=ls())
 source('D:/git/header.R')
 
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# data is downloaded from:
+# https://www.kaggle.com/fivethirtyeight/uber-pickups-in-new-york-city
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#temp
+importdir <- 'D:/Users/NCho/data'
+
 # collect all filename
 importfolder <- paste0(importdir,'/kaggle/uber-pickups-in-new-york-city')
 flist <- list.files(importfolder,'*.csv')
@@ -10,3 +18,5 @@ uber14 <- do.call("rbind", lapply(paste0(importfolder,'/', flist[grep("uber.*14"
                             read_csv,
                             col_types = cols(
                             'Date/Time' = col_datetime(format = '%m/%d/%Y %H:%M:%S'))))
+
+uber_all <- lapply(paste0(importfolder,'/', flist), function(x) cbind(read_csv(x), fname = basename(x)))
