9d858beed1a5eaea49da82ae6ded01a9ad6c054a
Neil <cjw.neil@gmail.com>
2018-01-18 00:33:34 -0500

    Preliminary election stats almost done.

--- politics/19thelec_korpres/01_import_data.R
+++
@@ -1,9 +0,0 @@
-cat("\014")
-rm(list=ls())
-source("D:/git/header.R")
-
-library(readxl)
-filename <- "_19.xlsx"
-temp<-read_xlsx(paste0(importdir,"/projects/politics/19thelec_korpres/",filename), 
-          sheet = 1, 
-          col_names = TRUE)
